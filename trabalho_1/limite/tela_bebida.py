5e5b87d92939034a9ab33855fba9318fa6e413fc
davibomf <58120196+davibomf@users.noreply.github.com>
2024-05-18 18:10:11 -0300

    Add files via upload

--- trabalho_1/limite/tela_bebida.py
+++ trabalho_1/limite/tela_bebida.py
@@ -16,31 +16,34 @@ class TelaBebida():
   def pega_dados_bebida(self):
     print("-------- DADOS BEBIDA ----------")
     nome = input("Nome: ")
-    custo = input("Custo: ")
     preco = input("Preço: ")
     percent_comissao = input("Comissão: ")
-    codigo = input("Código: ")
-    veget = input("Vegetariano: ")
-    vegan = input("Vegano: ")
-    gluten = input("Gluten: ")
-    lactose = input("Lactose: ")
+#    veget = input("Vegetariano: ")
+#    vegan = input("Vegano: ")
+#    gluten = input("Gluten: ")
+#    lactose = input("Lactose: ")
     grau_alcoolico = input("Grau alcoolico: ")
+    ingrediente1 = int(input("Código do ingrediente 1: "))
+    ingrediente2 = int(input("Código do ingrediente 2: "))
+    ingrediente3 = int(input("Código do ingrediente 3: "))
 
-    return {"nome": nome, "custo": custo, "preco": preco,
-            "percent_comissao": percent_comissao, "codigo": codigo, "veget": veget,
-            "vegan": vegan, "gluten": gluten, "lactose": lactose,
-            "grau_alcoolico": grau_alcoolico}
+    return {"nome": nome, "preco": preco,
+            "percent_comissao": percent_comissao,
+            #"veget": veget,
+            #"vegan": vegan, "gluten": gluten, "lactose": lactose,
+            "ingrediente1": ingrediente1, "ingrediente2": ingrediente2,
+            "ingrediente3": ingrediente3, "grau_alcoolico": grau_alcoolico}
 
   def mostra_bebida(self, dados_bebida):
     print("NOME DO PRATO: ", dados_bebida["nome"])
     print("CUSTO DO PRATO: ", dados_bebida["custo"])
     print("PRECO DO PRATO: ", dados_bebida["preco"])
     print("COMISSÃO DO PRATO: ", dados_bebida["percent_comissao"])
-    print("CÓDIGO DO PRATO: ", dados_bebida["codigo"])
-    print("VEGETARIANO: ", dados_bebida["veget"])
-    print("VEGANO: ", dados_bebida["vegan"])
-    print("GLUTEN: ", dados_bebida["gluten"])
-    print("LACTOSE: ", dados_bebida["lactose"])
+#    print("CÓDIGO DO PRATO: ", dados_bebida["codigo"])
+#    print("VEGETARIANO: ", dados_bebida["veget"])
+#    print("VEGANO: ", dados_bebida["vegan"])
+#    print("GLUTEN: ", dados_bebida["gluten"])
+#    print("LACTOSE: ", dados_bebida["lactose"])
     print("\n")
 
   # fazer aqui tratamento dos dados, caso a entrada seja diferente do esperado
--- trabalho_1/limite/tela_suprimento.py
+++ trabalho_1/limite/tela_suprimento.py
@@ -3,9 +3,10 @@ class TelaSuprimento():
         print('---------- SUPRIMENTOS ----------')
         print('Escolha a opção:')
         print('1 - Criar novo suprimento')
-        print('2 - Atualizar qtd de suprimento')
+        print('2 - Atualizar suprimento')
         print('3 - Listar suprimentos')
         print('4 - Excluir suprimento')
+        print('0 - Retornar')
 
         opcao = int(input('Escolha a opção: '))
         return opcao
@@ -13,18 +14,22 @@ class TelaSuprimento():
     def pega_dados_suprimento(self):
         print('---------- DADOS DO SUPRIMENTO -----------')
         nome = input('Nome do suprimento: ')
-        qtd = input('Quantidade do suprimento: ')
-        codigo = input('Código do suprimento: ')
+        qtd = int(input('Quantidade do suprimento: '))
+        preco = int(input('Preço do suprimento: '))
 
-        return {"codigo": codigo, "nome": nome, "qtd": qtd}
+        return {"nome": nome, "qtd": qtd, "preco": preco}
     
     def mostra_suprimento(self, dados_suprimento):
-        print("CODIGO DO SUPRIMENTO: ", dados_suprimento["codigo"])
         print("NOME DO SUPRIMENTO: ", dados_suprimento["nome"])
         print("QUANTIDADE DO SUPRIMENTO: ", dados_suprimento["qtd"])
+        print("PRECO DO SUPRIMENTO: ", dados_suprimento["preco"])
+        print("CODIGO DO SUPRIMENTO: ", dados_suprimento["codigo"])
         print("\n")
 
     def seleciona_suprimento(self):
-        codigo = input("Código do suprimento que deseja selecionar: ")
+        codigo = int(input("Código do suprimento que deseja selecionar: "))
         return codigo
+    
+    def mostra_mensagem(self, msg):
+        print(msg)
 
