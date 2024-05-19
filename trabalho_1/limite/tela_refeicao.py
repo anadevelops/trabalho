0c8996445f269a4064ea7f99626f9263343a9030
davibomf <58120196+davibomf@users.noreply.github.com>
2024-05-19 01:05:50 -0300

    Add files via upload

--- trabalho_1/limite/tela_refeicao.py
+++ trabalho_1/limite/tela_refeicao.py
@@ -6,7 +6,7 @@ class TelaRefeicao():
     print("Escolha a opcao")
     print("1 - Incluir Refeição")
     print("2 - Alterar Refeição")
-    print("3 - Listar Refeições")
+    print("3 - Listar Refeição")
     print("4 - Excluir Refeição")
     print("0 - Retornar")
 
@@ -16,34 +16,36 @@ class TelaRefeicao():
   def pega_dados_refeicao(self):
     print("-------- DADOS REFEIÇÃO ----------")
     nome = input("Nome: ")
-    custo = input("Custo: ")
     preco = input("Preço: ")
-    percent_comissao = input("Comissão: ")
-    codigo = input("Código: ")
-    veget = input("Vegetariano: ")
-    vegan = input("Vegano: ")
-    gluten = input("Gluten: ")
-    lactose = input("Lactose: ")
+#    veget = input("Vegetariano: ")
+#    vegan = input("Vegano: ")
+#    gluten = input("Gluten: ")
+#    lactose = input("Lactose: ")
+    ingrediente1 = int(input("Código do ingrediente 1: "))
+    ingrediente2 = int(input("Código do ingrediente 2: "))
 
-    return {"nome": nome, "custo": custo, "preco": preco,
-            "Comissão": percent_comissao, "codigo": codigo, "veget": veget,
-            "vegan": vegan, "gluten": gluten, "lactose": lactose}
+    return {"nome": nome, "preco": preco,
+            #"veget": veget,
+            #"vegan": vegan, "gluten": gluten, "lactose": lactose,
+            "ingrediente1": ingrediente1, "ingrediente2": ingrediente2}
 
   def mostra_refeicao(self, dados_refeicao):
     print("NOME DO PRATO: ", dados_refeicao["nome"])
-    print("CUSTO DO PRATO: ", dados_refeicao["custo"])
+    #print("CUSTO DO PRATO: ", dados_bebida["custo"])
     print("PRECO DO PRATO: ", dados_refeicao["preco"])
-    print("COMISSÃO DO PRATO: ", dados_refeicao["percent_comissao"])
+    print("INGREDIENTE 1: ", dados_refeicao["ingrediente1"])
+    print("INGREDIENTE 2: ", dados_refeicao["ingrediente2"])
     print("CÓDIGO DO PRATO: ", dados_refeicao["codigo"])
-    print("VEGETARIANO: ", dados_refeicao["veget"])
-    print("VEGANO: ", dados_refeicao["vegan"])
-    print("GLUTEN: ", dados_refeicao["gluten"])
-    print("LACTOSE: ", dados_refeicao["lactose"])
+#    print("CÓDIGO DO PRATO: ", dados_bebida["codigo"])
+#    print("VEGETARIANO: ", dados_bebida["veget"])
+#    print("VEGANO: ", dados_bebida["vegan"])
+#    print("GLUTEN: ", dados_bebida["gluten"])
+#    print("LACTOSE: ", dados_bebida["lactose"])
     print("\n")
 
   # fazer aqui tratamento dos dados, caso a entrada seja diferente do esperado
   def seleciona_refeicao(self):
-    codigo = input("Código da refeição que deseja selecionar: ")
+    codigo = int(input("Código da refeição que deseja selecionar: "))
     return codigo
 
   def mostra_mensagem(self, msg):
